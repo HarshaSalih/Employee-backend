c1a25ae3005defe5ce49ad22604829c231771ab9
HarshaSalih <harshasalih2000@gmail.com>
2022-12-05 15:38:46 +0530

    commits

---
+++ src/main/java/com/example/employeeapp_backend/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package com.example.employeeapp_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class EmployeeController {
+
+    @GetMapping("/")
+    public String Employeewelcomepage()
+    {
+        return "Welcome to my website";
+    }
+}
