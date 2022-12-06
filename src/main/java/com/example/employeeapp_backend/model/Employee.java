02838582ddc5d352fcb306692f10c626cbc594d2
HarshaSalih <harshasalih2000@gmail.com>
2022-12-06 15:16:43 +0530

    bugfix commits

--- src/main/java/com/example/employeeapp_backend/model/Employee.java
+++ src/main/java/com/example/employeeapp_backend/model/Employee.java
@@ -1,9 +1,6 @@
 package com.example.employeeapp_backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="Employee")
@@ -11,7 +8,7 @@ import jakarta.persistence.Table;
 public class Employee {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue (strategy = GenerationType.AUTO)
     private int id;
 
     private int empcode;
